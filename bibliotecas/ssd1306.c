5bd2044d5bb600d600b449a1a71480c0436c9d72
Luis Eduardo F. S. Pereira <eduardo38sao3@live.com>
2025-02-09 17:29:06 -0300

    Implementação do código para uso do display, e modificação do código sdd1306.c para conseguir exibir os caracteres no display.

--- bibliotecas/ssd1306.c
+++ bibliotecas/ssd1306.c
@@ -93,8 +93,6 @@ void ssd1306_fill(ssd1306_t *ssd, bool value) {
     }
 }
 
-
-
 void ssd1306_rect(ssd1306_t *ssd, uint8_t top, uint8_t left, uint8_t width, uint8_t height, bool value, bool fill) {
   for (uint8_t x = left; x < left + width; ++x) {
     ssd1306_pixel(ssd, x, top, value);
@@ -156,25 +154,27 @@ void ssd1306_vline(ssd1306_t *ssd, uint8_t x, uint8_t y0, uint8_t y1, bool value
 // Função para desenhar um caractere
 void ssd1306_draw_char(ssd1306_t *ssd, char c, uint8_t x, uint8_t y)
 {
-  uint16_t index = 0;
-  char ver=c;
-  if (c >= 'A' && c <= 'Z')
-  {
-    index = (c - 'A' + 11) * 8; // Para letras maiúsculas
-  }else  if (c >= '0' && c <= '9')
-  {
-    index = (c - '0' + 1) * 8; // Adiciona o deslocamento necessário
-  }
-  
-  for (uint8_t i = 0; i < 8; ++i)
-  {
-    uint8_t line = font[index + i];
-    for (uint8_t j = 0; j < 8; ++j)
-    {
-      ssd1306_pixel(ssd, x + i, y + j, line & (1 << j));
+    uint16_t index = 0;
+
+    if (c >= '0' && c <= '9') {
+        index = (c - '0' + 1) * 8;
+    } else if (c >= 'A' && c <= 'Z') {
+        index = (c - 'A' + 11) * 8;
+    } else {
+        return; // Não desenha caracteres inválidos
     }
-  }
+
+    for (uint8_t i = 0; i < 8; ++i) {
+        uint8_t line = font[index + i];
+        for (uint8_t j = 0; j < 8; ++j) {
+            ssd1306_pixel(ssd, x + i, y + j, (line >> j) & 1);
+        }
+    }
+
+    // Garante que o display seja atualizado após desenhar o caractere
+    ssd1306_send_data(ssd);
 }
+
 
 // Função para desenhar uma string
 void ssd1306_draw_string(ssd1306_t *ssd, const char *str, uint8_t x, uint8_t y)
--- comunicacao-serial.c
+++ comunicacao-serial.c
@@ -5,6 +5,17 @@
 #include "bibliotecas/ssd1306.h"
 #include "bibliotecas/font.h"
 
+const uint8_t led_red_pino = 11;
+const uint8_t led_blue_pino = 12;
+const uint8_t led_green_pino = 13;
+volatile bool estado_red = false;
+volatile bool estado_blue = false;
+volatile bool estado_green = false;
+
+const uint8_t btn_a = 5;
+const uint8_t btn_b = 6;
+const uint8_t btn_j = 22;
+
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
@@ -30,6 +41,16 @@ int main()
 
     bool cor = true;
     while (true) {
-
+        if(stdio_usb_connected){
+            char tecla;
+            if(scanf("%c", &tecla) == 1){
+                cor = !cor;
+                // Atualiza o conteúdo do display com animações
+                ssd1306_fill(&ssd, !cor); // Limpa o display
+                ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+                ssd1306_draw_char(&ssd, tecla, 20, 30);
+                ssd1306_send_data(&ssd); // Atualiza o display
+            }
+        }
     }
 }
